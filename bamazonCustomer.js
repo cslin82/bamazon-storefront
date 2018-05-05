e259c88d20c6d9cfdd46f3f08bec12c69f589e43
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-05-05 00:03:41 -0400

    Add skeleton sequelize logic to customer

---
+++ bamazonCustomer.js
@@ -0,0 +1,60 @@
+const SQLUSER = 'bamazon_cust';
+const SQLPASS = 'dYOVIR6ZRjH+LMf6A3BX';
+
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('bamazon', SQLUSER, SQLPASS, {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+const Product = sequelize.define('product', {
+    // item_id (unique id for each product)
+    item_id: Sequelize.INTEGER,
+    // product_name (Name of product)
+    product_name: Sequelize.STRING,
+    // department_name
+    department_name: Sequelize.STRING,
+
+    // price (cost to customer)
+    price: Sequelize.DECIMAL,
+
+    // stock_quantity (how much of the product is available in stores)
+    stock_quantity: Sequelize.INTEGER
+
+});
+
+
+sequelize.sync()
+    .then(() => Product.create({
+        product_name: 'apples',
+        department_name: 'produce',
+        price: 3,
+        stock_quantity: 100
+    }))
+    .then(thisthing => {
+        console.log(thisthing.toJSON());
+    });
+
+
+
+
+// Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+
+
+
+
+// The app should then prompt users with two messages.
+// The first should ask them the ID of the product they would like to buy.
+// The second message should ask how many units of the product they would like to buy.
+
+
+// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+
+// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
+
+// However, if your store does have enough of the product, you should fulfill the customer's order.
+
+// This means updating the SQL database to reflect the remaining quantity.
+// Once the update goes through, show the customer the total cost of their purchase.
+
---
+++ testprompts.js
@@ -0,0 +1,16 @@
+const inquirer = require('inquirer');
+
+const questions = [
+    /* Pass your questions in here */
+    {
+       
+
+    }
+];
+
+
+inquirer.prompt(questions).then(answers => {
+    console.log(answers);
+    
+
+});
